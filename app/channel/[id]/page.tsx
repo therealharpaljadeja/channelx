521651282682a6460cca12aa089c881df7c76999
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-05-14 13:11:36 +0530

    feat: add splash screen

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -343,8 +343,8 @@ export default function ChannelDetailsPage() {
                             </>
                         )
                     ) : (
-                        <div className="flex flex-col w-full h-full flex-1 justify-center">
-                            <div className="flex flex-col h-full items-center bg-[url('/slide.svg'),_url('/pattern.svg')] bg-[length:100%_100%] bg-[position:10px_-25px] p-4">
+                        <div className="flex flex-col w-full h-full flex-1 justify-center bg-[url('/slide.svg'),_url('/pattern.svg')] bg-[length:100%_100%]">
+                            <div className="flex flex-col h-full items-center  bg-[position:10px_-25px] p-4">
                                 <img src="/token.svg" />
                                 <Button
                                     onClick={startStream}
@@ -357,7 +357,7 @@ export default function ChannelDetailsPage() {
                         </div>
                     )
                 ) : (
-                    <div className="flex flex-col w-full h-full flex-1 justify-center">
+                    <div className="flex flex-col w-full h-full flex-1 bg-[url('/slide.svg'),_url('/pattern.svg')]  bg-[length:100%_100%] ">
                         <div className="flex flex-col h-full items-center bg-[url('/slide.svg'),_url('/pattern.svg')]  bg-[length:100%_100%] bg-[position:10px_-25px] p-4">
                             <img src="/token.svg" />
                             <ConnectButton />
--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,36 @@ export default function Login() {
     if (authenticated) return router.push("/app");
 
     return (
-        <div className="flex flex-col space-y-8">
-            <div className="flex space-x-2 items-center justify-center">
-                <img src="/lock.svg" className="w-8" />
-                <img src="/ChannelX.svg" className="w-36" />
+        <div className="w-full h-full bg-[#371a6c] overflow-hidden flex-1 flex relative flex-col justify-center">
+            <img
+                src="/Token.png"
+                className="w-24 h-24 absolute top-24 left-4"
+            />
+            <img
+                src="/Token.png"
+                className="w-28 h-28 absolute top-[-24px] left-72"
+            />
+            <img
+                src="/Token.png"
+                className="w-36 h-36 absolute top-36 left-48"
+            />
+
+            <div className="flex flex-1 flex-col space-y-8 justify-center items-center">
+                <div className="flex space-x-2 mt-36 items-center justify-center">
+                    <img src="/ChannelX-logo.svg" className="w-36" />
+                </div>
+                <Button
+                    colorScheme="purple"
+                    color="white"
+                    onClick={login}
+                    leftIcon={<img className="h-4 w-4" src="/farcaster.svg" />}
+                >
+                    Sign in with Farcaster
+                </Button>
             </div>
-            <Button
-                colorScheme="purple"
-                color="white"
-                onClick={login}
-                leftIcon={<img className="h-4 w-4" src="/farcaster.svg" />}
-            >
-                Sign in with Farcaster
-            </Button>
+            <div className="bg-transparent flex justify-center w-full p-12 bg-[url('/Stream.svg')] bg-cover bg-[position:56px_48px]">
+                <img src="/FBI.svg" />
+            </div>
         </div>
     );
 }
